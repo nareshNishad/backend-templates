feebb41741a4625cf341b0d15973e29a25c8a195
naresh-zota <naresh@zotalabs.com>
2021-01-05 22:10:21 +0530

    package update

--- graphql without apollo/app.js
+++ graphql without apollo/app.js
@@ -22,13 +22,14 @@ app.use(
   })
 );
 
+const PORT = process.env.PORT || 8000;
 mongoose
   .connect(
     `mongodb+srv://main_admin:Ubl3jwePbunuMDU4@mobile-app-backend.dxs48.mongodb.net/SellIt-mobile-app?retryWrites=true&w=majority`,
     { useNewUrlParser: true }
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((err) => {
     console.log("DB Connection error", err);
