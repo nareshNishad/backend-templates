7ae97f3a70b37e075d5e50e0c0714490432b2510
naresh-zota <naresh@zotalabs.com>
2021-01-06 08:29:21 +0530

    cors enabled

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const graphQlSchema = require("./graphql/schema/index");
 const graphQlResolvers = require("./graphql/resolvers/index");
 const isAuth = require("./middleware/is-auth");
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -21,7 +24,7 @@ app.use(
     graphiql: true,
   })
 );
-
+const PORT = process.env.PORT || 8000;
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mobile-app-backend.dxs48.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
